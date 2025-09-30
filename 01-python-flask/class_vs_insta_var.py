71dee929b3c7799fa24aa2ecc47be284227ce0b9
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-09-30 13:09:31 +0530

    Fix some typos

--- 01-python-flask/class_vs_insta_var.py
+++ 01-python-flask/class_vs_insta_var.py
@@ -1,19 +1,19 @@
-class empolyee:
+class employee:
     companyName="Microsoft"
-    # this is the examle of the class variable
+    # this is the example of the class variable
     noofEmployee=0
     def __init__(self,name):
         self.name=name
         self.raise_amount=0.54
-        empolyee.noofEmployee+=1
+        employee.noofEmployee+=1
     def showdetails(self):
-        print(f"The name of the empolyee is {self.name} and the raise amount is {self.raise_amount} of the sized {self.noofEmployee} in the comapny {self.companyName}")
-emp1=empolyee("MR.India")
+        print(f"The name of the employee is {self.name} and the raise amount is {self.raise_amount} of the sized {self.noofEmployee} in the comapny {self.companyName}")
+emp1=employee("MR.India")
 emp1.showdetails()
-emp2=empolyee("Conquror")
+emp2=employee("Conquror")
 emp2.raise_amount=5.224
 emp2.companyName="Google"
 # this is the examle of the instance variable
 emp2.showdetails()
-# empolyee.showdetails(emp1) it is the same thing 
+# employee.showdetails(emp1) it is the same thing 
 
