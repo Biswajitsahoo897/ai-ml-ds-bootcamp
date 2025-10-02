861fef5210453740560ca6207bbce30a9e628230
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-10-02 16:30:37 +0530

    Add example

--- 01-python-flask/warlusoperator_1.py
+++ 01-python-flask/warlusoperator_1.py
@@ -2,3 +2,8 @@ numbers = [1, 2, 3, 4, 5]
 while (n := len(numbers)) > 0:
     print(numbers.pop())
 
+# Example 
+while (data:=input("Enter the Value:")):
+    print(data)
+    if(data=="q" or data=="Q"):
+        break
