b62e20ab0f280d9b6410373e904233874856ab92
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-10-02 16:29:42 +0530

    Add shell utility module or shutil

---
+++ 01-python-flask/os_modules-inpython/shutil_module_2.py
@@ -0,0 +1,11 @@
+# Shutil is a Python module that provides a higher level interface for working with file and directories. 
+# The name "shutil" is short for shell utility. 
+# It provides a convenient and efficient way to automate tasks that are commonly performed on files and directories. 
+# In this repl, we'll take a closer look at the shutil module and its various functions and how they can be used in Python.
+
+
+
+import shutil
+print("This will copy to another file")
+shutil.copy2("C:\\Users\\biswa\OneDrive\Desktop\coding\.vscode\PYTHON\shutil_module_1.py","C:\\Users\\biswa\OneDrive\Desktop\coding\.vscode\PYTHON\shutil_module_2.py")
+
